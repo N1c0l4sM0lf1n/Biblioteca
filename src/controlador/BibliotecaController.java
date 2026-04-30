38ae322bbab9203ec64a41b7aa6a5592e63c940d
dci <n.molfinqueo01@ufromail.cl>
2026-04-30 10:22:33 -0400

    Clase BibliotecaController

--- src/controlador/BibliotecaController.java
+++ src/controlador/BibliotecaController.java
@@ -1,4 +1,41 @@
 package controlador;
 
+import modelo.*;
+
+import java.util.List;
+
 public class BibliotecaController {
+    private Biblioteca biblioteca;
+
+    public BibliotecaController(Biblioteca biblioteca) {
+        this.biblioteca = biblioteca;
+    }
+
+    public void registrarLibro(String codigo, String titulo, String autor) {
+        biblioteca.agregarLibro(new Libro(codigo, titulo, autor));
+    }
+
+    public void registrarUsuario(String id, String nombre, String correo) {
+        biblioteca.agregarUsuario(new Usuario(id, nombre, correo));
+    }
+
+    public void prestarLibro(String idUsuario, String codigoLibro) {
+        biblioteca.registrarPrestamo(idUsuario, codigoLibro);
+    }
+
+    public void devolverLibro(String codigoLibro) {
+        biblioteca.devolverLibro(codigoLibro);
+    }
+
+    public List<Libro> obtenerLibros() {
+        return biblioteca.getLibros();
+    }
+
+    public List<Usuario> obtenerUsuarios() {
+        return biblioteca.getUsuarios();
+    }
+
+    public List<Prestamo> obtenerPrestamosActivos() {
+        return biblioteca.getPrestamosActivos();
+    }
 }
