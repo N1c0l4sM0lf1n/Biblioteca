b2770629695eff711a4c4986a72bc840c7fc84ac
dci <n.molfinqueo01@ufromail.cl>
2026-04-30 10:21:50 -0400

    Clase Usuario

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,4 +1,22 @@
 package modelo;
 
 public class Usuario {
+    private String id;
+    private String nombre;
+    private String correo;
+
+    public Usuario(String id, String nombre, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + nombre + " - " + correo;
+    }
 }
